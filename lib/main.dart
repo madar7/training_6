e8a127ef127c8abe906c9bf4643fd341e9f642cc
ibrahim hassan <ibrahimhassan@ibrahims-MacBook-Pro.local>
2025-01-20 14:05:29 +0300

    training_6 source code

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(firstPage());
+}
+
+class firstPage extends StatefulWidget {
+  const firstPage({super.key});
+
+  @override
+  State<firstPage> createState() => _firstPageState();
+}
+
+class _firstPageState extends State<firstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: CircleAvatar(
+                  radius: 80.0,
+                  backgroundImage: NetworkImage(
+                      'https://media.istockphoto.com/id/517401259/photo/her-own-kind-of-beautiful.jpg?s=1024x1024&w=is&k=20&c=OuGtIjrit6AHIB2ze7wOaLZeF5ggPf6ASPkduSk_hxM=')),
+            ),
+            Text(
+              'Ipruz',
+              style: TextStyle(
+                fontSize: 50.0,
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'APP DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.teal[100],
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              width: 100.0,
+              height: 50.0,
+              child: Divider(
+                color: Colors.teal[100],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 25,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+252 907 730868',
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 20,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'Ipruz@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
